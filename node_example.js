3a068cb5156e596400efec42223139ac25943bb2
Francisco Cabezas <fcabezas@jumpitt.com>
2018-10-10 21:07:47 -0300

    - Added comments

--- node_example.js
+++ node_example.js
@@ -10,19 +10,26 @@ var server = http.createServer(function (request, response) {
     input.forEach(function(numero) {
       var splitted = numero.split('=')[1]
       if(isNumeric(splitted)) {
+        //Ok, it's a number
         var number = Number(splitted)
         suma += number
       } else {
+        //It's not a number, so we show an error message and sends to the client
         response.writeHead(400, {'Content-Type': 'application/json'})
         response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
       }
     })
+    //If everithing was OK, we send code 200 response, using json format
     response.writeHead(200, {'Content-Type': 'application/json'})
     response.end(JSON.stringify({result:suma}))
   }
 })
 
+/**
+ ** isNumeric returns true if input is a number. This can be done using the oppsite behaviour from isNaN function
+ **/
 function isNumeric(num) {
+  //isNaN returns false if the input is a number, true otherwise
   return !isNaN(num)
 }
 
