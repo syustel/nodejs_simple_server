871e43248cd3f97a969167fdb374ca6e87366143
Sebastian Yuste <sebastian.yute@sansano.usm.cl>
2018-10-24 12:36:51 -0300

    mutliplicacon&fibonacci

--- node_example.js
+++ node_example.js
@@ -23,6 +23,50 @@ var server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'application/json'})
     response.end(JSON.stringify({result:suma}))
   }
+  else if(data[0] === '/multiplicacion') {
+    var producto = 1
+    var input = data[1].split('&')
+    input.forEach(function(numero) {
+      var splitted = numero.split('=')[1]
+      if(isNumeric(splitted)) {
+        //Ok, it's a number
+        var number = Number(splitted)
+        producto *= number
+      } else {
+        //It's not a number, so we show an error message and sends to the client
+        response.writeHead(400, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+      }
+    })
+    //If everithing was OK, we send code 200 response, using json format
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify({result:producto}))
+  }
+  else if(data[0] === '/fibonacci') {
+    var n1 = 0
+    var n2 = 1
+    var secuencia = []
+    var input = data[1].split('&')
+    var n = (input[0].split('=')[1])
+    if(isNumeric(n)) {
+        //Ok, it's a number
+        var number = Number(n)
+        var i
+        for (i = 0; i < n; i++) {
+          n2 = n2 + n1
+          n1 = n2 - n1
+          secuencia.push(n1)
+        }
+      } else {
+        //It's not a number, so we show an error message and sends to the client
+        response.writeHead(400, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify({result:'error, el input debe ser numerico'}))
+      }
+
+    //If everithing was OK, we send code 200 response, using json format
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify({result:secuencia}))
+  }
 })
 
 /**
